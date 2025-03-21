247b0729e8b2abf6bb7b69a4bb263f493151c19f
jasonsoprovich <190052169+jasonsoprovich@users.noreply.github.com>
2025-03-20 19:46:26 -0600

    feat: add basic add-book-to-table functionality

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const myLibrary = [];
 
-
 function Book(id, title, author, pages) {
   // take params, create a book then store it in the array
   if (!new.target) {
@@ -16,10 +15,9 @@ function Book(id, title, author, pages) {
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
-}
-
-function displayTable() {
-  // build book table listing
+  // console.log('title', book.title);
+  // console.log('author', book.author);
+  // console.log('pages', book.pages);
 }
 
 function createBook() {
@@ -27,14 +25,51 @@ function createBook() {
   const title = prompt('Enter Book Title');
   const author = prompt('Enter Book Author');
   const pages = prompt('Enter Book Pages');
-  const addBook = new Book(id, title, author, pages);
-  addBookToLibrary(addBook)
+  const newBook = new Book(id, title, author, pages);
+  addBookToLibrary(newBook);
+  displayBook(newBook);
   // const readStatus = prompt('Book Read (yes/no)', readStatus);
   // update display table??
 }
 
 const addBookButton = document.getElementById('add-book-button');
+const tableDiv = document.getElementById('table-container');
+const bookTable = document.createElement('table');
 
 addBookButton.addEventListener('click', () => {
   createBook();
 });
+
+const headers = ['Title', 'Author', 'Pages'];
+const headerRow = createTableHeaders(headers);
+
+function createTableHeaders() {
+  const headerRow = document.createElement('tr');
+  headers.forEach(headerText => {
+    const headerCell = document.createElement('th');
+    headerCell.textContent = headerText;
+    headerRow.appendChild(headerCell);
+  });
+  return headerRow;  
+}
+
+bookTable.appendChild(headerRow);
+tableDiv.appendChild(bookTable);
+
+function displayBook(book) {
+  // build book table listing
+  const addRow = document.createElement('tr');
+  const titleCell = document.createElement('td');
+  const authorCell = document.createElement('td');
+  const pagesCell = document.createElement('td');
+
+  titleCell.textContent = book.title;
+  authorCell.textContent = book.author;
+  pagesCell.textContent = book.pages;
+
+  addRow.appendChild(titleCell);
+  addRow.appendChild(authorCell);
+  addRow.appendChild(pagesCell);
+
+  bookTable.appendChild(addRow);
+}
